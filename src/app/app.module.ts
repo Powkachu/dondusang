8783e094e7f3b0feffb71edbe40dc1d45d96d849
Jonathan Hurter <john@scalingo.com>
2017-03-18 19:51:27 +0100

    Add scann

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { SignUpComponent } from '../pages/sign-up/sign-up.component';
 import { SignUpService } from '../pages/sign-up/sign-up.service';
 import { BadgesComponent } from '../pages/badges/badges.component';
 import { BadgesService } from '../pages/badges/badges.service';
+import { ScannComponent } from '../pages/scann/scann.component';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { TabsPage } from '../pages/tabs/tabs';
     NeedsComponent,
     SignUpComponent,
     BadgesComponent,
+    ScannComponent,
     TabsPage
   ],
   imports: [
@@ -25,6 +27,7 @@ import { TabsPage } from '../pages/tabs/tabs';
     NeedsComponent,
     SignUpComponent,
     BadgesComponent,
+    ScannComponent,
     TabsPage
   ],
   providers: [
