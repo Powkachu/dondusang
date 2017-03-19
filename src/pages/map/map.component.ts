982dec3399338d07a397d405cdd023d7290267e2
Anthony Martin <martin.anthonyq@gmail.com>
2017-03-19 01:30:12 +0100

    Map implementee

--- src/pages/map/map.component.ts
+++ src/pages/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { OnInit } from '@angular/core';
 
 
@@ -9,12 +9,17 @@ import { NeedsComponent } from '../needs/needs.component';
 import { EntityService } from './entity.service';
 import { Entity } from '../../models/entity';
 
+declare var google;
 
 @Component({
   selector: 'map',
   templateUrl: 'map.html',
 })
-export class MapComponent implements OnInit {
+export class MapComponent {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   entity: Entity;
 
   constructor
@@ -25,28 +30,70 @@ export class MapComponent implements OnInit {
     ) { }
 
   getNearestEntity(): void {
-    let that : MapComponent = this;
+    let that: MapComponent = this;
+    let gotPosition = false;
     navigator.geolocation.getCurrentPosition(function (pos) {
-    
+      if (!gotPosition) {
+        gotPosition = true;
+      } else {
+        return;
+      }
       that.entityService.getNearest(pos.coords.longitude, pos.coords.latitude).subscribe(
-          res =>
-          {
+        res => {
+          if (that.entity == null) {
             that.entity = res;
-            console.log(res);
-          },
-          err =>
-          {
-              console.error(err);
+            that.createMap(pos.coords.latitude, pos.coords.longitude);
           }
+        },
+        err => {
+        }
       );
     },
-    function (err) {
-      console.log("Test");
-    }, null)
+      function (err) {
+      }, null)
   }
 
-  ngOnInit(): void {
-    this.getNearestEntity()
+  ionViewDidLoad() {
+    this.getNearestEntity();
+  }
+
+  createMap(latitude: number, longitude: number) {
+    let latLng = new google.maps.LatLng(latitude, longitude);
+    let mapOptions = {
+      center: latLng,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsDisplay.setMap(this.map);
+
+    directionsService.route({
+      origin: new google.maps.LatLng(latitude, longitude),
+      destination:new google.maps.LatLng(this.entity.latitude, this.entity.longitude),
+      travelMode: google.maps.TravelMode.DRIVING
+    }, function (response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
   }
 
 }
